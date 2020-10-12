3e3670432b7d38cea9d06633c9067cc5a48cede9
AThooker <austintrent@icloud.com>
2020-10-12 15:15:09 -0400

    Add project files.

---
+++ IntroToAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using IntroToAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetAsync("http https://swapi.dev/api/people/1/").Result;
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Person personResponse = response.Content.ReadAsAsync<Person>().Result;
+                Console.WriteLine(personResponse.Name);
+
+                foreach(string vehicleURL in personResponse.Vehicles)
+                {
+                    HttpResponseMessage vehicleResponse = httpClient.GetAsync(vehicleURL).Result;
+                    Console.WriteLine(vehicleResponse.Content.ReadAsStringAsync().Result);
+                    Vehicle vehicle = vehicleResponse.Content.ReadAsAsync<Vehicle>().Result;
+                    Console.WriteLine(vehicle.Name);
+
+                }
+            }
+        }
+    }
+}
